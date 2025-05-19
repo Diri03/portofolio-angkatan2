53353a37fb3052b69632246e82a94951d422bfa9
Diri03 <diriansyah15@gmail.com>
2025-05-19 15:03:36 +0700

    Belajar PHP

---
+++ admin/php/1.php
@@ -0,0 +1,25 @@
+<?php 
+    echo "Test"; // string
+    print_r("tess"); // array
+    print "Test"; // string
+    var_dump("Tessss"); // string & tipe data
+    echo "<br><br>";
+
+    //variable
+    $nama = "Diriansyah";
+    echo $nama;
+    echo "<br>";
+    $umur = 26;
+    echo $umur;
+    echo "<br>";
+    $tinggi_badan = 166.6;
+    echo $tinggi_badan;
+    echo "cm";
+    echo "<br>";
+
+    define("alamat", "Karet Jakarta Pusat"); // variable const
+    echo alamat;
+    echo "<br>";
+    const telp = "082297789349";
+    echo telp;
+?>
