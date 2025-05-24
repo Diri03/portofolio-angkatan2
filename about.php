3eca0f6a4d9ab98dcad17a6af0b829fa016cce8d
Diri03 <diriansyah15@gmail.com>
2025-05-24 15:16:05 +0700

    buat dinamis about

--- about.php
+++ about.php
@@ -111,33 +111,34 @@
             <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 content">
-            <h2>UI/UX Designer &amp; Web Developer.</h2>
-            <p class="fst-italic py-3">
+            <h2><?php echo isset($rowProfile["title"]) ? $rowProfile["title"] : "" ?></h2>
+            <!-- <p class="fst-italic py-3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
-            </p>
+            </p> -->
+            <p class="fst-italic py-3"> </p>
             <div class="row">
               <div class="col-lg-6">
                 <ul>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span><?php echo isset($rowProfile["birthday"]) ? $rowProfile["birthday"] : "" ?></span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span><?php echo isset($rowProfile["website"]) ? $rowProfile["website"] : "" ?></span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span><?php echo isset($rowProfile["phone"]) ? $rowProfile["phone"] : "" ?></span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span><?php echo isset($rowProfile["city"]) ? $rowProfile["city"] : "" ?></span></li>
                 </ul>
               </div>
               <div class="col-lg-6">
                 <ul>
                   <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>email@example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span><?php echo isset($rowProfile["degree"]) ? $rowProfile["degree"] : "" ?></span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span><?php echo isset($rowProfile["email"]) ? $rowProfile["email"] : "" ?></span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span><?php echo isset($rowProfile["freelance"]) ? $rowProfile["freelance"] : "" ?></span></li>
                 </ul>
               </div>
             </div>
-            <p class="py-3">
+            <!-- <p class="py-3">
               Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
               Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
-            </p>
+            </p> -->
           </div>
         </div>
 
